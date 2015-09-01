5527432af7aa0836b819af144a280191075c4595
Jasmine Ying <jasminay@usc.edu>
2015-09-01 01:33:18 -0700

    scaled down, flippers are better, added bumpers

---
+++ Assets/Scripts/bump.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bump : MonoBehaviour {
+
+	public GameObject ball;
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		foreach(ContactPoint contact in col.contacts){
+			ball.GetComponent<Rigidbody>().AddForce(-1*contact.normal*force, ForceMode.Impulse);
+		}
+	}
+}
--- Assets/Scripts/flipperController.cs
+++ Assets/Scripts/flipperController.cs
@@ -3,18 +3,44 @@ using System.Collections;
 
 public class flipperController : MonoBehaviour {
 
+	public GameObject ball;
+	public float flipperForce;
+	Collision currentCol;
+	bool colliding;
+
 	void Start ()
 	{
-		GetComponent<Rigidbody> ().maxAngularVelocity = 25; // make sluggish at first
+		colliding = false;
+		GetComponent<Rigidbody> ().maxAngularVelocity = 60;
 	}
 	
 	void Update ()
 	{
 		if (Input.GetKeyDown (KeyCode.RightShift) && this.tag.Contains("right")) {
 			GetComponent<Rigidbody> ().AddTorque (0, Time.deltaTime * 10000000, 0);
+			if(colliding){
+				foreach(ContactPoint contact in currentCol.contacts){
+					ball.GetComponent<Rigidbody>().AddForce(-1*contact.normal*flipperForce, ForceMode.Impulse);
+				}
+			}
 		}
 		else if(Input.GetKeyDown(KeyCode.LeftShift)&& this.tag.Contains("left")){
 			GetComponent<Rigidbody> ().AddTorque (0, Time.deltaTime * -10000000, 0);
+			if(colliding){
+				foreach(ContactPoint contact in currentCol.contacts){
+					ball.GetComponent<Rigidbody>().AddForce(-1*contact.normal*flipperForce, ForceMode.Impulse);
+				}
+			}
 		}
+	}
+
+	void OnCollisionEnter (Collision col){
+		colliding = true;
+		currentCol = col;
+	}
+
+	void OnCollisionExit(Collision col){
+		colliding = false;
+		currentCol = null;
 	}
 }
