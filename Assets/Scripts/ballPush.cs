8ddfe1bbddba37fa902a3cdf117375b42c4f1b2d
Jasmine Ying <jasminay@usc.edu>
2015-08-31 22:28:24 -0700

    initial commit

---
+++ Assets/Scripts/ballPush.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballPush : MonoBehaviour {
+
+	bool collided;
+	public GameObject ball;
+	float power;
+
+	// Use this for initialization
+	void Start () {
+		power = 0;
+		collided = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (collided && Input.GetKeyDown (KeyCode.Space)) {
+//			ball.GetComponent<Rigidbody> ().AddForce(0,0,-1500);
+//		}
+		if(collided && Input.GetKey(KeyCode.Space)){
+			Debug.Log ("+power");
+			power += 0.2f;
+			if(power > 80){
+				power = 80f;
+			}
+		}
+		if(collided && Input.GetKeyUp(KeyCode.Space)){
+			ball.GetComponent<Rigidbody> ().AddForce(0,0,-10*power);
+			power = 0;
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		collided = true;
+	}
+
+	void OnTriggerExit(Collider other){
+		collided = false;
+	}
+}
---
+++ Assets/Scripts/flipperController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flipperController : MonoBehaviour {
+
+	void Start ()
+	{
+		GetComponent<Rigidbody> ().maxAngularVelocity = 25; // make sluggish at first
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.RightShift) && this.tag.Contains("right")) {
+			GetComponent<Rigidbody> ().AddTorque (0, Time.deltaTime * 10000000, 0);
+		}
+		else if(Input.GetKeyDown(KeyCode.LeftShift)&& this.tag.Contains("left")){
+			GetComponent<Rigidbody> ().AddTorque (0, Time.deltaTime * -10000000, 0);
+		}
+	}
+}
