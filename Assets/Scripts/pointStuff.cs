61cadea936b0690328e380dc058767a37d664e02
Jasmine Ying <jasminay@usc.edu>
2015-09-01 17:56:46 -0700

    point things now working

--- Assets/Scripts/pointStuff.cs
+++ Assets/Scripts/pointStuff.cs
@@ -3,13 +3,57 @@ using System.Collections;
 
 public class pointStuff : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject otherPointThing1;
+	public GameObject otherPointThing2;
+	public float speed;
+	public bool isHit;
+	public bool isMoving;
+	Vector3 defaultPos;
+	Vector3 downPos;
+	Vector3 goingTowards;
+
 	void Start () {
-	
+		isHit = false;
+		isMoving = false;
+		defaultPos = transform.position;
+		downPos = new Vector3(transform.position.x, transform.position.y-0.74f, transform.position.z); 
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		if(isMoving){
+			transform.position = Vector3.MoveTowards (transform.position, goingTowards, speed);
+		}
+		if (transform.position.Equals(goingTowards)){
+			isMoving = false;
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		isHit = true;
+		goToDownPosition();
+		if(otherPointThing1.GetComponent<pointStuff>().isHit && otherPointThing2.GetComponent<pointStuff>().isHit){
+			Debug.Log("add 500");
+			StartCoroutine(popUp(5));
+		}
+		else{
+			Debug.Log("add 100");
+		}
+	}
+
+	IEnumerator popUp(float waitTime){
+		yield return new WaitForSeconds (waitTime);
+		otherPointThing1.GetComponent<pointStuff>().goToUpPosition();
+		otherPointThing2.GetComponent<pointStuff>().goToUpPosition();
+		goToUpPosition();
+	}
+
+	public void goToUpPosition(){
+		goingTowards = defaultPos;
+		isMoving = true;
+	}
+
+	public void goToDownPosition(){
+		goingTowards = downPos;
+		isMoving = true;
 	}
 }
